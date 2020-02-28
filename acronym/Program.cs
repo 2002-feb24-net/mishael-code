2900e290882c2cd5aa5a789b5ea5dfe55c16ad98
MishaelHarrison <mishael.harrison@gmail.com>
2020-02-28 09:28:14 -0600

    finished the acronym program

--- acronym/Program.cs
+++ acronym/Program.cs
@@ -6,7 +6,31 @@ namespace acronym
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string phrase = GetPhrase();
+            string acronym = FindAcronym(phrase);
+            System.Console.WriteLine("\n"+ phrase + " (" + acronym +")\n");
+        }
+
+        static string GetPhrase()
+        {
+            string output;
+
+            System.Console.Write("input a phrase: ");
+            output = Console.ReadLine();
+
+            return output;
+        }
+
+        static string FindAcronym(string phrase)
+        {
+            string output = "";
+            string[] words = phrase.Split(" ");
+            
+            for(int i=0;i<words.Length;i++) output+= words[i][0];
+
+            output = output.ToUpper();
+
+            return output;
         }
     }
 }
