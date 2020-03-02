3f729195111b8ec69861fddf3bdca5727b915a43
MishaelHarrison <mishael.harrison@gmail.com>
2020-03-02 14:05:25 -0600

    Finnished FizzBuzz

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,7 +6,26 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Initiateing FizzBuzz");
+
+            int fizzs = 0;
+            int buzzs = 0;
+            int fizzbuzzs = 0;
+            bool fizz;
+            bool buzz;
+
+            for (int i=1;i<=1000;i++)
+            {
+                fizz = i%3 == 0;
+                buzz = i%5 == 0;
+                fizzs += fizz&&!buzz?1:0;
+                buzzs += !fizz&&buzz?1:0;
+                fizzbuzzs += fizz&&buzz?1:0;
+                System.Console.Write((!fizz&&!buzz?(i+""):(fizz!=buzz?(fizz?"Fizz":"Buzz"):"FizzBuzz"))+"\t");
+                if(i%15 == 0) System.Console.WriteLine();
+            }
+
+            System.Console.WriteLine("\nFizz: "/*267*/ + fizzs + "\nBuzz: "/*134*/ + buzzs + "\nFizzBuzz: "/*66*/ + fizzbuzzs);
         }
     }
 }
